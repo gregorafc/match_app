398abff1c22a73e45f3b81498f7dd4115eda6c04
Grzegorz Suder <gregor.afc@gmail.com>
2014-09-04 12:24:54 +0200

    View for Event model, option delete event, option create new event for current_user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,23 @@
 class EventsController < ApplicationController
-
+  before_action :set_event, only: [:destroy]
 
   def index
     load_events
   end
 
+  def new
+    build_event
+  end
+
+  def create
+    build_event
+    save_event or render :new
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
+  end
 
   private
 
@@ -12,6 +25,24 @@ class EventsController < ApplicationController
     @events = current_user.events
   end
 
+  def build_event
+    @event ||= Event.new
+    @event.user_id = current_user.id
+    @event.attributes = event_params
+  end
+
+  def save_event
+    redirect_to events_path if @event.save   
+  end
+
+  def event_params
+    event_params = params[:event]
+    event_params ? event_params.permit(:game_name, :field_id) : {}
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
 
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+
+  def match_field_data(event)
+    "#{event.field.sportcenter.name} - #{event.field.name}"
+  end
+
+  def field_datas
+    lambda { |field| "#{field.sportcenter.name} - #{field.name}"}
+  end
+
 end
