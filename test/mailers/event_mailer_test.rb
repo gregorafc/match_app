351f1ecbbb218624b0d1d6ce70a3e532aab57cc1
Grzegorz Suder <gregor.afc@gmail.com>
2014-09-07 21:12:17 +0200

    Mailer test fix

--- test/mailers/event_mailer_test.rb
+++ test/mailers/event_mailer_test.rb
@@ -4,12 +4,18 @@ class EventMailerTest < ActionMailer::TestCase
   test "after_create_event" do
     event = FactoryGirl.create(:event)
     player = FactoryGirl.create(:player)
+    user = FactoryGirl.create(:user)
+    field = FactoryGirl.create(:field)
+    sportcenter = FactoryGirl.create(:sportcenter)
 
     event.players << player
+    user.events << event
+    sportcenter.fields << field
+    field.events << event
 
     mail = EventMailer.after_create_event(event)
     assert_equal "After create event", mail.subject
-    assert_equal ["#{player.email}"], mail.to
+    assert_equal player.email, mail.to
     assert_equal ["action.joe.doe@example.com"], mail.from
     #assert_match "Hi", mail.body.encoded
   end
