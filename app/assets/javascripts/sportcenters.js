8a6075bdb90814cf4033a84417715f9cbf8bf945
Grzegorz Suder <gregor.afc@gmail.com>
2014-09-20 22:55:43 +0200

    Ajax form create

--- app/assets/javascripts/sportcenters.js
+++ app/assets/javascripts/sportcenters.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+  var $button = $('#new_sportcenter'),
+      $page = $('#form-container');
+
+  $button.on('click', function(){
+    $.ajax('/sportcenters/new').success(function(data){
+      $(this).hide();
+      $page.html(data);
+    });
+
+
+
+  });
+});
