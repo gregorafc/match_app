e2195453cc816cb493d91184ea44de477977fdd2
Grzegorz Suder <gregor.afc@gmail.com>
2014-10-01 18:46:37 +0200

    Add ajax create form for new sportcenter

--- app/assets/javascripts/sportcenters.js
+++ app/assets/javascripts/sportcenters.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
 
-  var $button = $('#new_sportcenter'),
-      $page = $('#form-container');
+  var button = $('#new_sportcenter'),
+      page = $('#form-container');
 
-  $button.on('click', function(){
-    $.ajax('/sportcenters/new').success(function(data){
-      $(this).hide();
-      $page.html(data);
+  button.on('click', function(event){
+    $.ajax('/sportcenters/new', {
+      success: function(response){
+      button.hide();
+      page.html(response);
+      }
     });
-
-
-
   });
+
+  $('.back').on('click', function() {
+    page.html('');
+    button.show();
+  });
+
+
 });
